dd4c675ff4dd56fb3b96bcd7b5e8322a7e598f85
simpleauthority <jacob@algorithmjunkie.com>
2020-02-28 17:25:49 -0800

    add app vue

---
+++ vue/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hi! This blog is under construction.</h1>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
